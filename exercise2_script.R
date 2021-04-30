5e470b47bccb0ec26bc102d0d05c2551a9e10db3
Nicolas Steinmann <nicolas.steinmann@uzh.ch>
2021-04-30 12:13:02 +0200

    completed exercise 2

--- exercise2_script.R
+++ exercise2_script.R
@@ -5,4 +5,13 @@ library(sf)
 library(terra)        
 library(lubridate)
 
-test 
+#Task 2
+
+wildschwein_BE <- read_delim("data/wildschwein_BE_2056.txt", ",") %>%
+  st_as_sf(coords = c("E", "N"), crs = 2056, remove= FALSE) %>%
+  group_by(TierID) %>%
+  
+  mutate(steplength = sqrt((E - lead(E,1))^2 + (N - lead(N,1))^2))%>%
+  mutate(timediff = as.numeric(difftime(lead(DatetimeUTC), DatetimeUTC,1)))
+
+#Task 3
